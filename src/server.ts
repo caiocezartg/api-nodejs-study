30bd43eb27751479b85757a9d82e63fb2462220c
Caio <caiocezartg@gmail.com>
2023-03-27 15:05:42 -0300

    env: add env variables with validation

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .select('*')
+    .where('amount', 500)
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log(`Server listening!`)
