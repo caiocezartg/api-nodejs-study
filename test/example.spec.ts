47c124be4e174434668a0364eb1d3189034c1054
Caio <caiocezartg@gmail.com>
2023-04-03 15:39:47 -0300

    test: user capable of create transaction

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,21 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('user can create a new transaction', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  const response = await request(app.server).post('/transactions').send({
+    title: 'New transaction',
+    amount: 5000,
+    type: 'credit',
+  })
+
+  expect(response.statusCode).toEqual(201)
 })
